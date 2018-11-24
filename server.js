5047ce685a81d91a5bef78dfffbe6f78e5c8478c
Glitch (peat-sale) <none>
2018-11-24 00:21:00 +0000

    🐬🦁 Checkpoint ./server.js:2177980/1663 ./package.json:2177980/23 ./public/client.js:2177980/484 ./views/index.ejs:2177980/3310 ./public/style.css:2177980/294

--- server.js
+++ server.js
@@ -5,30 +5,28 @@
 const express = require('express');
 var app = express();
 
-app.use(express.static(_dirname + '/views'));
-app.use(express.static(_dirname + '/plublic'));
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get('/', (req, res) => {
+	var lang = req.headers["accept-language"].split(',')[0];
+	var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     (req.connection.socket ? req.connection.socket.remoteAddress : null);
+  var regexp = /\(([^)]+)\)/;
+  var os = regexp.exec(req.headers['user-agent'])[1];
+	res.render('index.ejs', {user: 
+                           {
+                              ipaddress: ip,
+                              language: lang,
+                              software: os
+                            }
+  });
 });
 
+const port = process.env.PORT || 3000;
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}`);
 });
